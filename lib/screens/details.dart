f279218e6f96434d7d4697a6918fbc186764c16c
heladegachi <hela.degachi@esprit.tn>
2021-07-18 10:33:19 +0100

    basic structure

---
+++ lib/screens/details.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class Details extends StatefulWidget {
+  @override
+  _DetailsState createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget> [],
+      )
+    );
+  }
+}
+
